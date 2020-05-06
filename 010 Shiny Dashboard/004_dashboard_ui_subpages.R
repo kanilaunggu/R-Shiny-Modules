1ec4e4ea02b870be93d663542c996946dc3cbddb
khanilaunggu <kanilaunggu@Kanis-MacBook-Pro.local>
2020-05-06 21:51:38 +0700

    Menaikkan file 004

---
+++ 010 Shiny Dashboard/004_dashboard_ui_subpages.R
@@ -0,0 +1,59 @@
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Dashboard R-Lang"),
+  dashboardSidebar(
+    # membuat menu
+    sidebarMenu(
+      # menu pertama data pohon
+      menuItem("Data Iris/Pohon", tabName = "iris", icon = icon("tree")),
+      # menu kedua data mobil
+      menuItem("Data Cars/Mobil", tabName = "cars", icon = icon("car"))
+    )
+  ),
+  dashboardBody(
+    # item Menu
+    tabItems(
+      # item pertama dari menu pertama tabname="iris"
+      tabItem("iris",
+  
+              #membuat box, 
+              box(
+                # perintah di bawah menhasilkan html:
+                # <div id="correlation_plot" class="shiny-plot-output" style="width: 100% ; height: 400px"></div>
+                plotOutput("correlation_plot"),width=8
+              ),
+              box(
+                #membuat dropdown diberi nama features dan label Features:
+                selectInput("features","Features:",
+                            #isi dari dropdown
+                            c("Sepal.Width","Petal.Length","Petal.Width")),width = 4
+              )                         
+      ),
+      # item kedua dari menu kedua tabname="cars"
+      tabItem("cars",
+         fluidPage(
+           h1("Cars")     
+         ) 
+
+              
+      )
+    )
+    
+    
+    
+
+  )
+  
+)
+
+server <- function(input,output) {
+  # mengambil id correlation_plot dengan bentuk output$correlation_plot
+  output$correlation_plot <- renderPlot({
+    plot(iris$Sepal.Length,iris[[input$features]],
+         xlab="Sepal length",ylab = "Feature")
+  })
+}
+
+shinyApp(ui,server)
